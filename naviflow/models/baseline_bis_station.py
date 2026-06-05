b6f0941c28d65a822c8177e918a6f9b3fed25df1
Clement Ribart <clement.ribart@gmail.com>
2026-06-05 09:40:13 +0200

    added comments

--- naviflow/models/baseline_bis_station.py
+++ naviflow/models/baseline_bis_station.py
@@ -6,8 +6,12 @@ def init_baseline_station():
 
     # $CHALLENGIFY_BEGIN
     model = models.Sequential()
+    #In X the column with index=1 = TARGET value = 'NB_VALD_TOTAL'
+    #With -1 we take the last value of the list = day before prediction
     model.add(layers.Lambda(lambda x: x[:,-1,1,None]))
 
+    #Optimizer is still necessary in this case
+    # because we build a Tensorflow 'Sequential' architecture
     adam = optimizers.Adam(learning_rate=0.02)
     model.compile(loss='mse', optimizer=adam, metrics=["mae"])
 
